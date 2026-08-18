ce29082255510b0eb6a9136cbe16163b49175c7b
Maria <maria_c_brito@estudante.sesisenai.org.br>
2026-08-18 11:47:17 -0300

    up - atualizei o index novamente

--- index.php
+++ index.php
@@ -29,50 +29,6 @@ $usuario = mysqli_query($conexao, "SELECT * FROM usuario");
             <button type="submit">Cadastrar</button>
         </form>
 
-        <h2>Adicione um novo prato</h2>
-        <form action="public/cadastrar.php" method="POST">
-            <label for="titulo">Nome do prato</label>
-            <input type="text" name="titulo">
-            <br>
-            <label for="autor">Descrição do prato</label>
-            <input type="text" name="descricao">
-            <br>
-            <label for="autor">Preço</label>
-            <input type="number" name="preco">
-            <label for="categoria">Categoria:</label>
-            <select name="categoria" id="categoria" required>
-                <option value="">Selecione uma categoria</option>
-                <option value="Entrada">Entrada</option>
-                <option value="Prato Principal">Prato Principal</option>
-                <option value="Sobremesa">Sobremesa</option>
-            </select>
-            <button type="submit">Cadastrar</button>
-        </form>
-        <div>
-            <h2>Livros Cadastrados</h2>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Título</th>
-                    <th>Autor</th>
-                    <th>Ano</th>
-                    <th>Ações</th>
-                </tr>
-                <?php while ($livro = mysqli_fetch_assoc($livros)) { ?>
-                    <tr>
-                        <td><?php echo $livro["id"] ?></td>
-                        <td><?php echo $livro["titulo"] ?></td>
-                        <td><?php echo $livro["autor"] ?></td>
-                        <td><?php echo $livro["ano"] ?></td>
-                        <td>
-                            <a href="public/editar.php?id=<?php echo $livro["id"] ?>">Editar</a>
-                            <a href="public/excluir.php?id=<?php echo $livro["id"] ?>">Excluir</a>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </table>
-        </div>
-
     </main>
     <footer>
 
