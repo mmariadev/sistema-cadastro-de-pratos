951686248dc15b85c7d85ed6b6dc2bac08c3d0fa
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-18 11:54:48 -0300

    add -  terminei o cadastrar_usuario.php

--- public/cadastrar_usuario.php
+++ public/cadastrar_usuario.php
@@ -2,3 +2,14 @@
 
 include "../infra/conexao.php";
 
+$nome_usuario = $_POST["nome_usuario"];
+$email_usuario = $_POST["email_usuario"];
+
+$sql = "INSERT INTO usuario (nome_usuario, email_usuario) VALUES (?, ?)";
+
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("ss", $nome_usuario, $email_usuario);
+$stmt->execute();
+
+header("location: ../index.php");
+?>
