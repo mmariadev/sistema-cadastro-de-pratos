53aad686e22d0c25fdb9385c28811dffc3617193
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-18 12:16:14 -0300

    add -  terminei cadastrar_pratos_dados.php

---
+++ public/cadastrar_pratos_dados.php
@@ -0,0 +1,16 @@
+<?php
+
+include "../infra/conexao.php";
+
+$nome_pratos = $_POST["nome_pratos"];
+$descricao_pratos = $_POST["descricao_pratos"];
+$precos_pratos = $_POST["precos_pratos"];
+$categoria_pratos = $_POST["categoria_pratos"];
+
+$sql = "INSERT INTO pratos (nome_pratos, descricao_pratos, precos_pratos, categoria_pratos) VALUE (?, ?, ?, ?)";
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("ssis", $nome_pratos, $descricao_pratos, $precos_pratos, $categoria_pratos);
+$stmt->execute();
+
+header("Location: cadastrar_pratos.php");
+?>
