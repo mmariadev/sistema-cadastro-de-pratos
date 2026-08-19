29fc89037b687b4acc2e165ff9ca1f9393d6e581
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-19 11:39:58 -0300

    add - terminei excluir_pratos.php

--- public/excluir_pratos.php
+++ public/excluir_pratos.php
@@ -5,3 +5,9 @@ include " ../infra/conexao.php";
 $id_pratos = $_GET["id_pratos"];
 
 $sql = "DELETE FROM pratos WHERE id_pratos = ?";
+
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("i", $id_pratos);
+$stmt->execute();
+
+header("Location: cadastrar_pratos.php");
